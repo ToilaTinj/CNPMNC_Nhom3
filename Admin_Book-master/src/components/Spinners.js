5c97f8b9461bf672e2631aa504a46dae0000271d
NguyenThanhTinh <117492769+ToilaTinj@users.noreply.github.com>
2024-10-11 13:15:16 +0700

    Delete Admin_Book-master directory

--- Admin_Book-master/src/components/Spinners.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Spinners(props) {
-    return (
-        <div class="spinner-border" role="status" style={{ height: '20px', width: '20px' }}> </div>
-    );
-}
-
-export default Spinners;
--- Admin_Book-master/src/constants/config.js
+++
@@ -1,2 +0,0 @@
-export const URL_API = 'http://localhost:5000/api'
-
