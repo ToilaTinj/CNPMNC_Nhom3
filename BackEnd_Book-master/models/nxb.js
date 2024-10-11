941a277f9ed08b30aa0f92cc071900fe0a4c160c
NguyenThanhTinh <117492769+ToilaTinj@users.noreply.github.com>
2024-10-11 13:15:24 +0700

    Delete BackEnd_Book-master directory

--- BackEnd_Book-master/models/nxb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const nxbSchema = new mongoose.Schema({
-    name: { type: String, require: true }
-}, { timestamps: true })
-
-const NXB = mongoose.model('nxb', nxbSchema);
-
-module.exports = NXB;
--- BackEnd_Book-master/models/type.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const typeSchema = new mongoose.Schema({
-    name: { type: String, require: true }
-}, { timestamps: true })
-
-const Type = mongoose.model('type', typeSchema);
-
-module.exports = Type;
